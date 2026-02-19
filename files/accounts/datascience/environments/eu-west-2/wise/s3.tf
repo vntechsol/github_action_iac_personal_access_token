19cb2e11aec4d3288a294a818fa4e0de1d0fe042
Linh Nguyen <linh5847@gmail.com>
2026-02-19 12:22:16 +0000

    test2

--- files/accounts/datascience/environments/eu-west-2/wise/s3.tf
+++ files/accounts/datascience/environments/eu-west-2/wise/s3.tf
@@ -1,5 +1,5 @@
 module "s3_bucket" {
-  source = "git::https://github.com/vntechsol/github_action_iac_ssh_key//modules/s3/bucket?ref=f2" # private repo with SSH and add ?ref=featurebranch name
+  source = "git::https://github.com/vntechsol/github_action_iac_ssh_key//modules/s3/bucket" # private repo with SSH and add ?ref=featurebranch name
 
   enabled        = true
   s3_bucket_name = "infra-test-s3bucket"
