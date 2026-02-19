18b9b550d432bbcdc5a02222371869d987c01228
Linh Nguyen <linh5847@gmail.com>
2026-02-19 12:04:29 +0000

    test with personal-access-token on github-action

--- files/accounts/datascience/environments/eu-west-2/wise/s3.tf
+++ files/accounts/datascience/environments/eu-west-2/wise/s3.tf
@@ -1,6 +1,6 @@
 module "s3_bucket" {
   #source = "git::ssh:https://github.com/vntechsol/github_action_iac_ssh_key//modules/s3/bucket" # private repo with HTTPS secrets.ASK_FOR_PASS
-  source = "git::ssh://git@github.com/vntechsol/github_action_iac_ssh_key//modules/s3/bucket?ref=main" # private repo with SSH and add ?ref=featurebranch name
+  source = "git::ssh://git@github.com/vntechsol/github_action_iac_ssh_key//modules/s3/bucket?ref=f2" # private repo with SSH and add ?ref=featurebranch name
 
   enabled        = true
   s3_bucket_name = "infra-test-s3bucket"
